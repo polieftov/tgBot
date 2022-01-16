554a32dc53ba56cbd64cdab7f4f96165cbad4d20
polieftov <polieftovegor@gmail.com>
2022-01-16 15:50:09 +0500

    убрал тест врайтера

--- TelegramBot/MyUpdateHandler.cs
+++ TelegramBot/MyUpdateHandler.cs
@@ -12,62 +12,53 @@ namespace TelegramBot
     class MyUpdateHandler : IUpdateHandler
     {
         private readonly ICommandsExecutor _executor;
+
         public MyUpdateHandler(ICommandsExecutor executor)
         {
             _executor = executor;
         }
 
-        public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception,
+            CancellationToken cancellationToken)
         {
             Console.WriteLine(exception.Message);
             Console.WriteLine(exception.StackTrace);
             AppDomain.CurrentDomain.UnhandledException += ProcessException;
-            
         }
-        
+
         static void ProcessException(object sender, UnhandledExceptionEventArgs args)
         {
             Console.WriteLine((args.ExceptionObject as Exception).StackTrace);
             Environment.Exit(1);
         }
-        
+
         private bool _scheduleGroup = false;
-        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+            CancellationToken cancellationToken)
         {
             var messageText = update.Message.Text;
             if (messageText != null)
             {
-                if (messageText == "kek")
+                if (_scheduleGroup)
                 {
-                    var d = new Dictionary<String, object>() { {"kok", (object)"https://metanit.com/sharp/tutorial/4.9.php"} };
-                    new WriterWithLinks(botClient).WriteAsync("loool", cancellationToken, update, d);
-
+                    var scheduleCmd = _executor.FindCommandByName("расписание");
+                    scheduleCmd.Execute(messageText, botClient, cancellationToken, update);
+                    _scheduleGroup = false;
+                    return;
                 }
+
+
+                var commandName = messageText.Split()[0]; //парсит текст комманды
+                if (commandName == "расписание")
+                    _scheduleGroup = true;
+                var cmd = _executor.FindCommandByName(commandName); // находит команду по имени
+                if (cmd != null)
+                    cmd.Execute(messageText, botClient, cancellationToken,
+                        update); //выполняет комаду, достает данные и отправляет пользователю
                 else
-                {
-
-
-
-                    if (_scheduleGroup)
-                    {
-                        var scheduleCmd = _executor.FindCommandByName("расписание");
-                        scheduleCmd.Execute(messageText, botClient, cancellationToken, update);
-                        _scheduleGroup = false;
-                        return;
-                    }
-
-
-                    var commandName = messageText.Split()[0]; //парсит текст комманды
-                    if (commandName == "расписание")
-                        _scheduleGroup = true;
-                    var cmd = _executor.FindCommandByName(commandName); // находит команду по имени
-                    if (cmd != null)
-                        cmd.Execute(messageText, botClient, cancellationToken,
-                            update); //выполняет комаду, достает данные и отправляет пользователю
-                    else
-                        await botClient.SendTextMessageAsync(chatId: update.Message.Chat.Id,
-                            text: "Не знаю такой команды");
-                }
+                    await botClient.SendTextMessageAsync(chatId: update.Message.Chat.Id,
+                        text: "Не знаю такой команды");
             }
         }
     }
