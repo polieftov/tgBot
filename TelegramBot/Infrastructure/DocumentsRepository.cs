b66a96977aed691fa68c791b87a03f86651f414c
polieftov <polieftovegor@gmail.com>
2022-01-16 15:54:47 +0500

    репо с документами и ссылками

---
+++ TelegramBot/Infrastructure/DocumentsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TelegramBot.Infrastructure
+{
+    public static class DocumentsRepository
+    {
+        public static readonly Dictionary<string, string> DocumentsWithUrl = new Dictionary<string, string>()
+        {
+            {"Все документы", "https://urfu.ru/ru/students/documents/"}
+        };
+
+    }
+}
